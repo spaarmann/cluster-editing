c6b7a129bb6f354ccf331c9dc81fcea5a59c3585
Sebastian Paarmann <sebastian@s-paarmann.de>
2020-12-29 15:34:09 +0100

    Make critical_cliques code use new Graph

--- src/critical_cliques.rs
+++ src/critical_cliques.rs
@@ -1,16 +1,11 @@
-use crate::PetGraph;
-
-use std::collections::HashSet;
-
-use petgraph::graph::NodeIndex;
+use crate::Graph;
 
 #[derive(Debug, Clone, Default)]
 pub struct CritClique {
     pub vertices: Vec<usize>,
-    pub node_indices: Vec<NodeIndex>,
 }
 
-pub fn find_crit_cliques(g: &PetGraph) -> Vec<CritClique> {
+pub fn find_crit_cliques(g: &Graph) -> Vec<CritClique> {
     let mut cliques = Vec::new();
 
     // TODO: This looks at least O(n^2) but should apparently be do-able in O(n + m), so have
@@ -18,43 +13,26 @@ pub fn find_crit_cliques(g: &PetGraph) -> Vec<CritClique> {
 
     let mut visited = vec![false; g.node_count()];
 
-    for u in g.node_indices() {
-        if visited[u.index()] {
+    for u in g.nodes() {
+        if visited[u] {
             continue;
         }
 
-        visited[u.index()] = true;
+        visited[u] = true;
         let mut clique = CritClique::default();
-        clique.node_indices.push(u);
-        clique.vertices.push(*g.node_weight(u).unwrap());
+        clique.vertices.push(u);
 
-        // We need to be able to compare neighborhoods, so we have to collect them into some
-        // collection. Since I'm not sure we can rely on a particular order being returned by
-        // `.neighbors`, use a `HashSet` for now.
-        // TODO: If we do transition to using a Graph representation that guarantees order of
-        // neighbors, it's probably more efficient to not use HashSets to store them and instead either
-        // use a Vec or just compare the iterators directly.
-        // TODO: If that doesn't happen, it'd be nice to at least not recompute them multiple
-        // times, but the borrow checker has been making that a bit annoying.
-
-        let neighbors = std::iter::once(u)
-            .chain(g.neighbors(u))
-            .collect::<HashSet<_>>();
-
-        for v in g.node_indices() {
-            if visited[v.index()] {
+        for v in g.nodes() {
+            if visited[v] {
                 continue;
             }
 
-            let v_neighbors = std::iter::once(v)
-                .chain(g.neighbors(v))
-                .collect::<HashSet<_>>();
+            // TODO: Is it maybe worth storing neighbor sets instead of recomputing them?
 
-            if neighbors == v_neighbors {
-                clique.node_indices.push(v);
-                clique.vertices.push(*g.node_weight(v).unwrap());
+            if g.closed_neighbors(u).eq(g.closed_neighbors(v)) {
+                clique.vertices.push(v);
 
-                visited[v.index()] = true;
+                visited[v] = true;
             }
         }
 
