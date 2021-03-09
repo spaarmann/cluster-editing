cfa9d800412041735c9c94390f50a66b302efa2b
Sebastian Paarmann <sebastian@s-paarmann.de>
2021-03-09 10:58:59 +0100

    Fix runner possibly blocking solver processes, stopping progress

--- src/tools/runner.rs
+++ src/tools/runner.rs
@@ -1,8 +1,9 @@
 use std::error::Error;
 use std::fs::File;
-use std::io::{self, Write};
+use std::io::{BufRead, BufReader, Write};
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
+use std::sync::mpsc;
 use std::time::{Duration, Instant};
 
 use log::info;
@@ -107,6 +108,42 @@ fn do_file(
         .spawn()
         .unwrap();
 
+    let stdout = child.stdout.take().unwrap();
+    let stderr = child.stderr.take().unwrap();
+
+    // So, apparently, I/O is still quite annoying, even in 2021.
+    // We need to keep reading from stdout & stderr of the child process
+    // because otherwise that may eventually block on writing to them, but
+    // we obviously want it to continue calculating stuff.
+    // But we can't *read* from them without blocking ourselves in the process,
+    // which would prevent us from handling the timeout properly.
+    // So we spawn two threads that read stdout and stderr as available, blocking
+    // while doing so, and send the data into a channel.
+    // To make things a bit easier, we can treat the channel as having an infinite
+    // buffer, so we just wait for the child to either exit or time out and then
+    // read the channels in their entirety at once.
+
+    let stdout_rx = {
+        let (tx, rx) = mpsc::channel();
+        std::thread::spawn(move || {
+            let reader = BufReader::new(stdout);
+            for line in reader.lines() {
+                tx.send(line.unwrap()).unwrap();
+            }
+        });
+        rx
+    };
+    let stderr_rx = {
+        let (tx, rx) = mpsc::channel();
+        std::thread::spawn(move || {
+            let reader = BufReader::new(stderr);
+            for line in reader.lines() {
+                tx.send(line.unwrap()).unwrap();
+            }
+        });
+        rx
+    };
+
     let result = match child
         .wait_timeout(Duration::from_secs(timeout * 60))
         .unwrap()
@@ -127,20 +164,25 @@ fn do_file(
         }
     };
 
-    let mut stdout = child.stdout.take().unwrap();
-    let mut stderr = child.stderr.take().unwrap();
-
     let mut output_path = out_dir.clone();
     output_path.push(format!("{}.out", filename));
     let mut output_file = File::create(output_path).unwrap();
 
     output_file.write_all("== stdout ==\n".as_bytes()).unwrap();
-    io::copy(&mut stdout, &mut output_file).unwrap();
+
+    while let Ok(line) = stdout_rx.recv() {
+        writeln!(output_file, "{}", line).unwrap();
+    }
+    //io::copy(&mut stdout, &mut output_file).unwrap();
 
     output_file
         .write_all("\n== stderr ==\n".as_bytes())
         .unwrap();
-    io::copy(&mut stderr, &mut output_file).unwrap();
+
+    while let Ok(line) = stderr_rx.recv() {
+        writeln!(output_file, "{}", line).unwrap();
+    }
+    //io::copy(&mut stderr, &mut output_file).unwrap();
 
     output_file
         .write_all(format!("\n== time ==\nElapsed time: {}", result.1).as_bytes())
